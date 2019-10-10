af66e6f59f2e2a206f374dcab08d14089f8acf7b
Kai Zhang <woshizk@outlook.com>
2019-10-10 01:56:26 +0100

    VaR and CVaR updated (Normal and t-stats)

--- ANALYSIS/Analysis_130603_191005.py
+++ ANALYSIS/Analysis_130603_191005.py
@@ -1,6 +1,6 @@
 import numpy as np
-import scipy as sp
 import pandas as pd
+from scipy.stats import norm, t
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style("whitegrid")
@@ -20,54 +20,77 @@ class VaR(object):
             mu is mean value
             sigma is variance
         """
-        self.rets         = rets
-        self.mu           = rets.mean()
-        self.sigma        = rets.std()
         self.__seed__     = 42
         self.__n_sample__ = 1000000
+        self.rets         = rets
+        self.nu           = None
+        self.mu           = None
+        self.sigma        = None
 
-    def Calculate_VaR(self, P, c):
-        VAR_param = self.VaR_Param(P, c, self.mu, self.sigma)
-        VAR_hist  = self.VaR_Hist(P, c, self.rets)
-        VAR_simu  = self.VaR_Simu(P, c, self.mu, self.sigma, self.__seed__, self.__n_sample__)
+    def VaR_norm(self, alpha):
+        self.mu, self.sigma = norm.fit(self.rets)
+        VAR_param = self.VaR_Param(alpha, self.mu, self.sigma)
+        VAR_hist  = self.VaR_Hist(alpha, self.rets)
+        VAR_simu  = self.VaR_Simu(alpha, self.mu, self.sigma, self.__seed__, self.__n_sample__)
 
         VAR_table = pd.DataFrame({"Paremetric VAR": VAR_param,
                                   "Historical VAR": VAR_hist,
-                                  "Simulated VAR": VAR_simu}, index = {"Confidence {0:.1f}%".format(c)})
+                                  "Simulated VAR": VAR_simu}, index = {"Confidence {0:.1f}%".format(100-alpha*100)})
 
         return VAR_table
 
-    @classmethod
-    def VaR_Param(cls, P, c, mu, sigma):
-        return P * sp.stats.norm.ppf(1 - c/100, mu, sigma)
+    def VaR_t(self, alpha, h):
+        """ h is number of days """
+        self.nu, self.mu, self.sigma = t.fit(self.rets)
+        VAR = self.VaR_t_stats(alpha, h, self.nu, self.mu, self.sigma)
+        VaR_table = pd.DataFrame({"VaR (t-statistics)": VAR}, index = {"Confidence {0:.1f}%".format(100-alpha*100)})
+        return VaR_table
 
     @classmethod
-    def VaR_Hist(cls, P, c, rets):
-        return P * np.percentile(rets, 100 - c)
+    def VaR_Param(cls, alpha, mu, sigma):
+        return norm.ppf(1-alpha, mu, sigma)
 
     @classmethod
-    def VaR_Simu(cls, P, c, mu, sigma, seed, n_sample):
+    def VaR_Hist(cls, alpha, rets):
+        return -np.percentile(rets, alpha*100)
+
+    @classmethod
+    def VaR_Simu(cls, alpha, mu, sigma, seed, n_sample):
         np.random.seed(seed)
         n_sims   = n_sample
-        rets_sim = np.random.normal(mu, sigma, n_sims)
-        return P * np.percentile(rets_sim, 100 - c)
+        rets_sim = np.random.normal(mu, n_sims, sigma)
+        return -np.percentile(rets_sim, alpha*100)
+
+    @classmethod
+    def VaR_t_stats(cls, alpha, h, nu, sigma, mu):
+        return np.sqrt((nu-2)/nu) * t.ppf(1 - alpha, nu) * sigma - h*mu
 
 class CVaR(VaR):
     def __init__(self, rets):
         super().__init__(rets)
-        self.VaR = self.Calculate_VaR(1000, 99)
 
-        print(self.CVaR_Param(0.01, self.mu, self.sigma))
+    def CVaR_norm(self, alpha):
+        self.mu, self.sigma = norm.fit(self.rets)
+        CVAR = self.CVaR_Normal(alpha, self.mu, self.sigma)
+        return pd.DataFrame({"CVaR (t-statistics)": CVAR}, index = {"Confidence {0:.1f}%".format(100-alpha*100)})
+
+    def CVaR_t(self, alpha, h):
+        """ h is number of days """
+        self.nu, self.mu, self.sigma = t.fit(self.rets)
+        self.nu = np.round(self.nu)
+        xanu    = t.ppf(alpha, self.nu)
+        CVAR    = self.CVaR_t_stats(alpha, 1, self.nu, xanu, self.mu, self.sigma)
+        return pd.DataFrame({"CVaR (t-statistics)": CVAR}, index = {"Confidence {0:.1f}%".format(100-alpha*100)})
 
     @classmethod
     def CVaR_Normal(cls, alpha, mu, sigma):
-        cvar = sp.stats.norm.pdf(sp.stats.norm.ppf(alpha)) * 1/alpha * sigma - mu
+        cvar = 1/alpha * norm.pdf(norm.ppf(alpha)) * sigma - mu
         return cvar
 
     @classmethod
-    def CVaR_t_stats(cls, alpha, mu, sigma):
-
-
+    def CVaR_t_stats(cls, alpha, h, nu, xanu, mu, sigma):
+        cvar = -1/alpha * 1/(1 - nu) * (nu - 2 + xanu**2) * t.pdf(xanu, nu) * sigma - h*mu
+        return cvar
 
 if __name__ == "__main__":
     import sys
@@ -77,5 +100,7 @@ if __name__ == "__main__":
     df      = df.rename(columns = {"index": "Time"}).set_index("Time")
     df_rets = df.apply(lambda x: Calculate_Return(x)).dropna()
 
-    # print(VaR(df_rets[["GBP_USD"]]).Calculate_VaR(1000, 99))
-    print(CVaR(df_rets[["GBP_USD"]]))
+    # print(VaR(df_rets[["GBP_USD"]]).VaR_norm(0.01))
+    print(VaR(df_rets[["GBP_USD"]]).VaR_t(0.01, 1))
+    print(CVaR(df_rets[["GBP_USD"]]).CVaR_norm(0.01))
+    print(CVaR(df_rets[["GBP_USD"]]).CVaR_t(0.01, 1))
